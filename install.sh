ebb4ffaae93f397e685e6d994209139e519344e7
Francis J. Sun <francisjsun@outlook.com>
2023-12-15 10:48:08 +1100

    fix git completion issue on OSX

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ fi
 
 if [ "$(uname -s)" = "Darwin" ]; then
     echo "$RUN_FS_BASHRC" >>~/.bash_profile
+    # install git completion for Darwin as well
+    cp -vf ./git-completion.bash ~
+    echo "
+if [ -f ~/git-completion.bash ]; then
+    . ~/git-completion.bash
+fi
+" >>~/.bash_profile
 else
     echo "$RUN_FS_BASHRC" >>~/.bashrc
 fi
